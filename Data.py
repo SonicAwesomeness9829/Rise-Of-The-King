7c6c5636ec0469cd63281f372a977f08d39197d2
Bruce <bruceawaterman@gmail.com>
2026-04-21 18:28:02 -0500

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,92 @@
+import time
+Game = {
+    "Game": True
+}
+Days = {
+    "Days": 0
+}
+print("YOU ARE THE KING OF...")
+kingdom = input("> ").strip().title()
+if kingdom == "Hell":
+    print("The true kingdom.")
+print("AND YOUR NAME IS!")
+name = input(">").strip().title()
+if name == "Satan":
+    print("True")
+if name == "Jesus":
+    print("False")
+if name == "Sonic":
+    print("Your name is now eggman for saying that")
+    name = "Eggman"
+if name == "Lucius":
+    print("No")
+    name = "Idiot"
+if name == "elijah" or name == "Elijah":
+    time.sleep(1)
+    print("Yep, now I know whose playing. Also could you have chosen something more original?")
+if name == "Gukesh":
+    print("I am honored!")
+print(f"Glory to the king of {kingdom}!") #I will watch it burn
+Kingdom_Properties = {
+    "Houses": 0,
+    "Arrow Towers": 0,
+    "Water Stations": 0,
+    "Farms": 0,
+    "Castle Hull": 0,
+    "Kingdom Hull": 0,
+    "Cannon Tower": 0,
+    "Multi Spuck Shot Tower": 0,
+    "Titanium Cannon Tower": 0,
+    "Titanium Power Station": 0,
+    "Titanium Kingdom Hull": 0,
+    "Titanium Castle Hull": 0,
+    "Merch Store": 0,
+    "Wood Farm": 0,
+    "Iron Generator": 0,
+    "Titanium Generator": 0
+}
+
+Kingdom_Inhabitants = {
+    "Peasants": 0,
+    "Troops": 0,
+    "Great Knight": 0,
+    "Power": 1
+}
+
+Kingdom_Assets = {
+    "Gold": 5000000,
+    "Land": 500000,
+    "Wood": 0,
+    "Iron": 0,
+    "Titanium":  0
+}
+HELL = {
+    "HELL": False
+}
+ENEMY = {
+    "Group": "Placeholder",
+    "Amount": 0,
+    "Power": 1
+}
+The_Devil = {
+    "HATE": 1                         # this number will go higher, don't worry
+}
+Castle = {
+    "Castle": False,
+    "Castle Power": 0
+}
+Reputation = {
+    "Grados": "Fine",
+    "Verdania": "Fine",
+    "Crystalia": "Fine",
+    "Hayden": "Glad",
+    "Julian": "Disgusted",
+    "Hell": "HATE"
+}
+peacekingdoms = {
+    "Verdania": False,
+    "Julian": False,
+    "Grados": False,
+    "Hayden": False,
+    "Crystalia": False
+}
